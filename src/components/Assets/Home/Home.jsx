049395c7640d63d379dcd64320e2171d7bffdfbb
dug0004 <DanielGarcia20@my.unt.edu>
2025-06-04 17:19:03 -0500

    Added login button and main title

--- src/components/Assets/Home/Home.jsx
+++ src/components/Assets/Home/Home.jsx
@@ -1,9 +1,13 @@
 import React, {useState} from 'react'
 import './Home.css'
- 
+import { useNavigate } from 'react-router-dom'
 
 const Home = () => {
   const [action, setAction] = useState("Hello")
+  navigation = useNavigate();
+  const loginButton = () => {
+    navigation('/')
+  }
   return (
     <div>
     <div className="navbar">
@@ -13,13 +17,17 @@ const Home = () => {
                 <li>Home</li>
                 <li>About</li>
                 <li>Contact</li>
+                <div className="loginbutton" onClick={()=> {loginButton()}}>Login</div>
             </ul>
         </div>
     </div>
+    
     <div className='content'>
+      
     <div className="rotating">
-      <h1 onClick={() => {action==="Hello"?setAction("Hi"): setAction("Hello")}}>{action}</h1>
+      <h1 onClick={() => {action==="Hello"?setAction("Hola"): setAction("Hello")}}>{action}</h1>
     </div>
+    <div className="title"><h1>Daniel's</h1></div>
     </div>
 
     </div>
