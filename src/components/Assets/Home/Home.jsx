a6b3fd202e0c5025336c30417520aeb856373a90
dug0004 <DanielGarcia20@my.unt.edu>
2025-06-03 21:18:59 -0500

    added cool animation

--- src/components/Assets/Home/Home.jsx
+++ src/components/Assets/Home/Home.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './Home.css'
- const Home = () => {
+ 
+
+const Home = () => {
+  const [action, setAction] = useState("Hello")
   return (
+    <div>
     <div className="navbar">
         <div className="navdiv">
             <div className="logo">Welcome</div>
@@ -11,6 +15,13 @@ import './Home.css'
                 <li>Contact</li>
             </ul>
         </div>
+    </div>
+    <div className='content'>
+    <div className="rotating">
+      <h1 onClick={() => {action==="Hello"?setAction("Hi"): setAction("Hello")}}>{action}</h1>
+    </div>
+    </div>
+
     </div>
   )
 }
